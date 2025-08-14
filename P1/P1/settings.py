be0364fd9f1c2ebd5325f067a632e28690bdbc71
code-with-vasu <agvasu2015@gmail.com>
2025-08-14 19:44:18 +0530

    settings file changed

--- P1/P1/settings.py
+++ P1/P1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+nuf@ar0se65^ibe%jfs)2apoie^)j_7(xn77rnvl7-0b^4%f+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agvasu1.pythonanywhere.com']
 
 
 # Application definition
